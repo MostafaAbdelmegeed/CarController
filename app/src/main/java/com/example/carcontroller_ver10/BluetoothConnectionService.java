6f81436b617a843d4380435aacb8b50b702d40f5
Mustafa Asaad <mostafa0as3d@gmail.com>
2019-11-05 13:07:26 +0200

    First commit!

---
+++ app/src/main/java/com/example/carcontroller_ver10/BluetoothConnectionService.java
@@ -0,0 +1,207 @@
+package com.example.carcontroller_ver10;
+
+import android.app.ProgressDialog;
+
+import android.bluetooth.BluetoothDevice;
+
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.UUID;
+
+
+public class BluetoothConnectionService {
+    private final String TAG = this.getClass().getSimpleName();
+    Context mContext;
+    private ConnectThread mConnectThread;
+    private BluetoothDevice mmDevice;
+    private static final UUID MY_UUID_INSECURE= UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private UUID deviceUUID;
+    private ConnectedThread mConnectedThread;
+
+
+
+    public BluetoothConnectionService(Context context ) {
+        mContext = context;
+    }
+
+
+
+
+    public void startClient(BluetoothDevice device, UUID uuid){
+        Log.d(TAG, "startClient: Started...");
+
+        mConnectThread = new ConnectThread(device, uuid);
+        mConnectThread.start();
+    }
+
+
+    /**
+     * This thread runs while attempting to make an outgoing connection
+     * with a device. It runs straight through; the connection either
+     * succeeds or fails.
+     */
+    private class ConnectThread extends Thread {
+        private BluetoothSocket mmSocket;
+
+        public ConnectThread(BluetoothDevice device, UUID uuid) {
+            Log.d(TAG, "ConnectThread: started.");
+            mmDevice = device;
+            deviceUUID = uuid;
+        }
+
+        public void run(){
+            BluetoothSocket tmp = null;
+            Log.i(TAG, "RUN mConnectThread ");
+
+            // Get a BluetoothSocket for a connection with the
+            // given BluetoothDevice
+            try {
+                Log.d(TAG, "ConnectThread: Trying to create InsecureRfcommSocket using UUID: "
+                        +MY_UUID_INSECURE );
+                tmp = mmDevice.createRfcommSocketToServiceRecord(deviceUUID);
+            } catch (IOException e) {
+                Log.e(TAG, "ConnectThread: Could not create InsecureRfcommSocket " + e.getMessage());
+            }
+
+            mmSocket = tmp;
+
+            // Make a connection to the BluetoothSocket
+
+            try {
+                // This is a blocking call and will only return on a
+                // successful connection or an exception
+                mmSocket.connect();
+
+                Log.d(TAG, "run: ConnectThread connected.");
+            } catch (IOException e) {
+                // Close the socket
+                try {
+                    mmSocket.close();
+                    Log.d(TAG, "run: Closed Socket.");
+                } catch (IOException e1) {
+                    Log.e(TAG, "mConnectThread: run: Unable to close connection in socket " + e1.getMessage());
+                }
+                Log.d(TAG, "run: ConnectThread: Could not connect to UUID: " + MY_UUID_INSECURE );
+            }
+
+            connected(mmSocket,mmDevice);
+        }
+        public void cancel() {
+            try {
+                Log.d(TAG, "cancel: Closing Client Socket.");
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "cancel: close() of mmSocket in Connect thread failed. " + e.getMessage());
+            }
+        }
+    }
+
+    /**
+     Finally the ConnectedThread which is responsible for maintaining the BTConnection, Sending the data, and
+     receiving incoming data through input/output streams respectively.
+     **/
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            Log.d(TAG, "ConnectedThread: Starting.");
+
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = mmSocket.getInputStream();
+                tmpOut = mmSocket.getOutputStream();
+                Log.d(TAG,"temp sockets created successfully");
+            } catch (IOException e) {
+                Log.e(TAG,"temp sockets not created", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run(){
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            //List<Patient> data;
+            int bytes; // bytes returned from read()
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                // Read from the InputStream
+                try {
+                    bytes = mmInStream.read(buffer);
+                    String incomingMessage = new String(buffer, 0, bytes);
+                    incomingMessage.trim();
+                    if (!incomingMessage.isEmpty()) {
+                        Log.d(TAG, "InputStream: " + incomingMessage);
+
+                        Intent incomingMessageIntent = new Intent("incomingMessage");
+                        incomingMessageIntent.putExtra("theMessage", incomingMessage);
+                        LocalBroadcastManager.getInstance(mContext).sendBroadcast(incomingMessageIntent);
+
+                        //data = new ArrayList<>();
+                        //data.add(new Patient(R.drawable.asset_0, "Mustafa As'ad", 1, 21, 80, 75, incomingMessage, 125, "+201028771928", "El Manial / EL Roda"));
+                        //Main2Activity.setPatients(data);
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "write: Error reading Input Stream. " + e.getMessage() );
+                    break;
+                }
+            }
+        }
+
+        //Call this from the main activity to send data to the remote device
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Log.e(TAG, "write: Error writing to output stream. " + e.getMessage() );
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+    private void connected(BluetoothSocket mmSocket, BluetoothDevice mmDevice) {
+        Log.d(TAG, "connected: Starting.");
+
+        // Start the thread to manage the connection and perform transmissions
+        mConnectedThread = new ConnectedThread(mmSocket);
+        mConnectedThread.start();
+    }
+
+    /**
+     * Write to the ConnectedThread in an unsynchronized manner
+     */
+    public void write(int mode) {
+        // Create temporary object
+        byte[] out = ByteBuffer.allocate(4).putInt(mode).array();
+        // Synchronize a copy of the ConnectedThread
+        Log.d(TAG, "write: Writing to outputstream: " + mode);
+        //perform the write
+        mConnectedThread.write(out);
+    }
+
+    public void cancel(){
+        mConnectedThread.cancel();
+    }
+
+}
---
+++ app/src/main/java/com/example/carcontroller_ver10/MainActivity.java
@@ -0,0 +1,320 @@
+package com.example.carcontroller_ver10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.drm.DrmStore;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.util.LruCache;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.ByteArrayOutputStream;
+import java.util.EventListener;
+import java.util.FormatFlagsConversionMismatchException;
+import java.util.UUID;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    private boolean POWER_ON = false;
+    private boolean AUTO_PILOT = false;
+
+
+    private ImageButton forwardBtn;
+    private ImageButton rightBtn;
+    private ImageButton reverseBtn;
+    private ImageButton leftBtn;
+    private ImageButton powerBtn;
+    private ImageButton autoBtn;
+
+    private TextView indicator;
+    private TextView rfid;
+
+    private int RFID_reading;
+
+    private final int  FORWARD = 8;
+    private final int  RIGHT = 6;
+    private final int  REVERSE = 2;
+    private final int  LEFT = 4;
+    private final int STOP = 5;
+
+
+    public BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    public static boolean CONNECTED = false;
+    public static final String CAR_MAC_ADDRESS = "98:D3:A1:FD:4B:1E";
+    public BluetoothDevice mBTDevice;
+    private static final UUID MY_UUID_INSECURE = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    public static StringBuilder messages = new StringBuilder();
+
+    BluetoothConnectionService bluetoothConnectionService;
+    public static final int AUTO_PILOT_MODE = 1;
+    public static final int REMOTE_CONTROLLED_MODE = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        forwardBtn = findViewById(R.id.forward);
+        rightBtn = findViewById(R.id.right);
+        reverseBtn = findViewById(R.id.reverse);
+        leftBtn = findViewById(R.id.left);
+        powerBtn = findViewById(R.id.power);
+        autoBtn = findViewById(R.id.auto);
+        indicator = findViewById(R.id.auto_indicator);
+        rfid = findViewById(R.id.rfid);
+
+        enableBT();
+
+        powerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!POWER_ON) {
+                    if(!CONNECTED){startCarBluetoothDevice();}
+                    startTheCar();
+                } else {
+                    stopTheCar();
+                    if(CONNECTED){stopCarBluetoothDevice();}
+                }
+            }
+        });
+
+        autoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!AUTO_PILOT) {
+                    switchToAutoPilotMode();
+                } else {
+                    switchToRemoteControlMode();
+                }
+            }
+        });
+
+
+        forwardBtn.setOnTouchListener(new RepeatListener(400, 100, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move(FORWARD);
+            }
+        }));
+
+
+        rightBtn.setOnTouchListener(new RepeatListener(400, 100, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move(RIGHT);
+            }
+        }));
+
+        reverseBtn.setOnTouchListener(new RepeatListener(400, 100, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move(REVERSE);
+            }
+        }));
+
+
+        leftBtn.setOnTouchListener(new RepeatListener(400, 100, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move(LEFT);
+            }
+        }));
+
+    }
+
+
+    /* ----------------------------------------------------- Methods --------------------------------------*/
+
+
+    private void startCarBluetoothDevice() {
+        mBTDevice = bluetoothAdapter.getRemoteDevice(CAR_MAC_ADDRESS);
+        bluetoothAdapter.cancelDiscovery();
+        bluetoothConnectionService = new BluetoothConnectionService(this.getApplicationContext());
+        bluetoothConnectionService.startClient(mBTDevice, MY_UUID_INSECURE);
+        CONNECTED = true;
+        toastForMe("Connected!");
+    }
+
+
+    private void stopCarBluetoothDevice() {
+        if (bluetoothAdapter != null) {
+            bluetoothConnectionService.cancel();
+            CONNECTED = false;
+        }
+    }
+
+    private void move(int direction){
+        bluetoothConnectionService.write(direction);
+    }
+
+
+    private void turnOnButtons() {
+        Log.d(TAG, "turnOnButtons: Turning on the buttons");
+        forwardBtn.setClickable(true);
+        forwardBtn.setAlpha(1.f);
+        rightBtn.setClickable(true);
+        rightBtn.setAlpha(1.f);
+        reverseBtn.setClickable(true);
+        reverseBtn.setAlpha(1.f);
+        leftBtn.setClickable(true);
+        leftBtn.setAlpha(1.f);
+        autoBtn.setClickable(true);
+        autoBtn.setAlpha(1.f);
+        indicator.setVisibility(View.VISIBLE);
+    }
+
+    private void turnOffButtons() {
+        Log.d(TAG, "turnOffButtons: Turning off the buttons");
+        forwardBtn.setClickable(false);
+        forwardBtn.setAlpha(0.2f);
+        rightBtn.setClickable(false);
+        rightBtn.setAlpha(0.2f);
+        reverseBtn.setClickable(false);
+        reverseBtn.setAlpha(0.2f);
+        leftBtn.setClickable(false);
+        leftBtn.setAlpha(0.2f);
+        autoBtn.setClickable(false);
+        autoBtn.setAlpha(0.2f);
+        indicator.setVisibility(View.INVISIBLE);
+    }
+
+    private void turnOnKeypad() {
+        Log.d(TAG, "turnOnButtons: Turning on the Keypad");
+        forwardBtn.setClickable(true);
+        forwardBtn.setAlpha(1.f);
+        rightBtn.setClickable(true);
+        rightBtn.setAlpha(1.f);
+        reverseBtn.setClickable(true);
+        reverseBtn.setAlpha(1.f);
+        leftBtn.setClickable(true);
+        leftBtn.setAlpha(1.f);
+    }
+
+    private void turnOffKeypad() {
+        Log.d(TAG, "turnOffButtons: Turning off the Keypad");
+        forwardBtn.setClickable(false);
+        forwardBtn.setAlpha(0.2f);
+        rightBtn.setClickable(false);
+        rightBtn.setAlpha(0.2f);
+        reverseBtn.setClickable(false);
+        reverseBtn.setAlpha(0.2f);
+        leftBtn.setClickable(false);
+        leftBtn.setAlpha(0.2f);
+    }
+
+
+    private void stopTheCar() {
+        Log.d(TAG, "stopTheCar: Stopping the car!");
+        switchToRemoteControlMode();
+        bluetoothConnectionService.write(STOP);
+        turnOffButtons();
+        POWER_ON = false;
+    }
+
+    private void switchToAutoPilotMode() {
+        indicator.setText("Auto Pilot: ON");
+        AUTO_PILOT = true;
+        bluetoothConnectionService.write(AUTO_PILOT_MODE);
+        turnOffKeypad();
+    }
+
+    private void switchToRemoteControlMode() {
+        indicator.setText("Auto Pilot: OFF");
+        AUTO_PILOT = false;
+        bluetoothConnectionService.write(REMOTE_CONTROLLED_MODE);
+        turnOnKeypad();
+    }
+
+    private void toastForMe(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+        Log.d(TAG, "toastForMe: Toast thrown for you!");
+    }
+
+
+    private void startTheCar() {
+        Log.d(TAG, "startTheCar: Starting the car!");
+        turnOnButtons();
+        POWER_ON = true;
+    }
+
+
+    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra("theMessage");
+            messages.replace(0, 0, text);
+            RFID_reading = Integer.parseInt(messages.toString());
+            rfid.setText(RFID_reading);
+            Log.d(TAG, "Incoming : " + text);
+        }
+    };
+
+    public void enableBT() {
+        if (bluetoothAdapter == null) {
+            Log.d(TAG, "enableDisableBT: Does not have BT capabilities.");
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "enableDisableBT: enabling BT.");
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+        } else {
+            Toast.makeText(this, "Connected to " + bluetoothAdapter.getBondedDevices(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /* --------------------------------------------------------------------------------------- */
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (POWER_ON)
+            stopTheCar();
+        if (bluetoothConnectionService != null)
+        stopCarBluetoothDevice();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (POWER_ON)
+            stopTheCar();
+    }
+
+
+}
+
+
+
